67d1733df3877c49e74bdacef073ac57890b1961
Duckfan77 <cwenthin@yahoo.com>
2020-12-01 10:17:47 -0600

    Solve Puzzle One
    Solved puzzle one, using a simple nested iterator.

--- puzzle1/src/main.rs
+++ puzzle1/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()>{
+    let mut file : File = File::open("input")?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
+
+    //create a Vec of i32 containing the elements of the input
+    let mut v: Vec<i32> = Vec::new();
+    for line in text.lines() {
+        v.push(line.parse::<i32>().unwrap_or(0));
+    }
+
+    for (i, el1) in v.iter().enumerate() {
+        for el2 in v[i+1..].iter() {
+            if el1 + el2 == 2020 {
+                println!("{}", el1 * el2);
+                return Ok(())
+            }
+        }
+    }
+
+    Ok(())
 }
