1efc0838fbf4cc8306c79b4fd7e55f855e960c93
Melissa <77photon@gmail.com>
2020-12-02 09:53:55 -0600

    Add comments
    Added comments to puzzle 3 and puzzle 4 to explain how parts work, removed a debug print from puzzle3

--- puzzle3/src/main.rs
+++ puzzle3/src/main.rs
@@ -10,31 +10,41 @@ fn main() -> std::io::Result<()>{
     let mut x = 0;
     for line in text.lines() {
         x += 1;
+        //get min character count
         let min = match line.find("-") {
             None => continue,
             Some(f) => f,
         };
         let minc = line[..min].parse::<i32>().unwrap_or(-1);
+
+        //get max character count
         let max = match line.find(" ") {
             None => continue,
             Some(f) => f,
         };
+
+        //get position of the colon
         let maxc = line[min+1..max].parse::<i32>().unwrap_or(-1);
         let colon = match line[max..].find(":") {
             None=>continue,
             Some(f) => f+max,
         };
+
+        //get the character to check for
         let c : char = match line[colon-1..colon].chars().next() {
             None => continue,
             Some(f) => f,
         };
+
+        //check every character in the line against the character c
         let mut ccount=0;
         for c2 in line[colon+1..].chars() {
             if c2 == c {
                 ccount+=1;
             }
         }
-        println!("ccount: {}, maxc: {}, minc: {}", ccount, maxc, minc);
+
+        //Check for valid number of characters
         if ccount <= maxc && ccount >= minc {
             n +=1;
         }
--- puzzle4/src/main.rs
+++ puzzle4/src/main.rs
@@ -10,26 +10,33 @@ fn main() -> std::io::Result<()>{
     let mut x = 0;
     for line in text.lines() {
         x += 1;
+        //get min character count
         let min = match line.find("-") {
             None => continue,
             Some(f) => f,
         };
         let minc = line[..min].parse::<i32>().unwrap_or(-1);
+
+        //get max character count
         let max = match line.find(" ") {
             None => continue,
             Some(f) => f,
         };
         let maxc = line[min+1..max].parse::<i32>().unwrap_or(-1);
+
+
         let colon = match line[max..].find(":") {
             None=>continue,
             Some(f) => f+max,
         };
+
+        //get position of the colon
         let c : char = match line[colon-1..colon].chars().next() {
             None => continue,
             Some(f) => f,
         };
-        let mut ccount=0;
 
+        let mut ccount=0;
         //check char 1, need to add 1 to account for the space
         if c == match line[colon +1+ minc as usize..].chars().next() {
             None=> continue,
@@ -46,6 +53,7 @@ fn main() -> std::io::Result<()>{
             ccount+=1;
         }
 
+        //Check for valid number of characters
         if ccount ==1 {
             n +=1;
         }
