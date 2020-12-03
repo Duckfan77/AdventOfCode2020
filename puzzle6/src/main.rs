a173dbd42c5f181771cdee74c07ca2414495de5c
Melissa <77photon@gmail.com>
2020-12-03 10:14:37 -0600

    Implement puzzle6
    Implemented puzzle6 using repeated iteration, once for each slope to check. The Right1Down2 slope is implemented separately, to accomadate the line skipping behavior. I may implement it using a single loop tracking all slopes simultaneously in the future.

--- puzzle6/src/main.rs
+++ puzzle6/src/main.rs
@@ -6,19 +6,48 @@ fn main() -> std::io::Result<()>{
     let mut text = String::new();
     file.read_to_string(&mut text)?;
 
-    let mut index : usize = 0;
-    let mut n = 0;
+    let mut index : usize;
+    let mut n;
+    let mut x : u64 = 1;
+    let slope = [1, 3, 5, 7];
 
+    //not the most efficient, iterate over full file for each slope to check
+    for s in slope.iter() {
+        n = 0;
+        index = 0;
+        //iterate over all lines, checking for collision with trees
+        for line in text.lines() {
+            if line[index..].chars().next().unwrap() == '#' {
+                n+=1;
+            }
+            index += s;
+            index %= line.len();
+        }
+        println!("Encountered {} trees on slope R: {}, D: 1", n, s);
+        x *= n;
+    }
+
+    //Check R1D2 slope
+    let mut skip = false;
+
+    n=0;
+    index = 0;
     //iterate over all lines, checking for collision with trees
-    for line in text.lines() {
+    for line in text.lines(){
+        skip = !skip;
+        if !skip {//skip line if skip is true, to get the down2 behavior
+            continue;
+        }
         if line[index..].chars().next().unwrap() == '#' {
             n+=1;
         }
-        index += 3;
+        index += 1;
         index %= line.len();
     }
+    println!("Encountered {} trees on slope R: 1, D: 2", n);
+    x*=n;
 
-    println!("Encountered {} trees", n);
+    println!("product of all tree counts: {}", x);
 
     Ok(())
 }
