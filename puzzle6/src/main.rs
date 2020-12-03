140cf4083de23e0aefe490c59c18c2b0c925ae3c
Melissa <77photon@gmail.com>
2020-12-03 09:50:42 -0600

    Copy puzzle5 to puzzle6

--- puzzle6/src/main.rs
+++ puzzle6/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()>{
+    let mut file : File = File::open("input")?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
+
+    let mut index : usize = 0;
+    let mut n = 0;
+
+    //iterate over all lines, checking for collision with trees
+    for line in text.lines() {
+        if line[index..].chars().next().unwrap() == '#' {
+            n+=1;
+        }
+        index += 3;
+        index %= line.len();
+    }
+
+    println!("Encountered {} trees", n);
+
+    Ok(())
 }
