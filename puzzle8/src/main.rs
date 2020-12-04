4c1034dbb741d04a278e03c14aaf7bab781ccee4
Melissa <77photon@gmail.com>
2020-12-04 10:24:47 -0600

    Implement Puzzle8
    Finished implementing puzzle 8 using regexes for all fields but ecl. The correct length of the field is checked, and then it is matched against a regex pattern to ensure it is correctly formatted. ecl simply uses a match statement as there are exactly 7 options to match against.

--- puzzle8/src/main.rs
+++ puzzle8/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
+#[macro_use] extern crate lazy_static;
+
+use regex::Regex;
 
 /**
  * Checks validity of byr field contents
@@ -8,7 +11,17 @@ use std::io::prelude::*;
  * Returns 1 if valid, 0 if invald
  */
 fn check_byr(field : &str) -> u8 {
-    return 1;
+    if field.len() != 4 {return 0;}
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^((19[2-9][0-9])|(200[0-2]))$").unwrap();
+    }
+    if RE.is_match(field) {
+        return 1
+    } else {
+        //println!("Didn't Match {} for byr", field);
+        return 0
+    }
+    //regex for year: ^((19[2-9][0-9])|(200[0-2]))$
 }
 
 /**
@@ -18,7 +31,17 @@ fn check_byr(field : &str) -> u8 {
  * Returns 1 if valid, 0 if invald
  */
 fn check_iyr(field : &str) -> u8 {
-    return 1;
+    if field.len() != 4 {return 0;}
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^((201[0-9])|2020)$").unwrap();
+    }
+    if RE.is_match(field) {
+        return 1
+    } else {
+        //println!("Didn't Match {} for iyr", field);
+        return 0
+    }
+    //regex for year: ^((201[0-9])|2020)$
 }
 
 /**
@@ -28,7 +51,17 @@ fn check_iyr(field : &str) -> u8 {
  * Returns 1 if valid, 0 if invald
  */
 fn check_eyr(field : &str) -> u8 {
-    return 1;
+    if field.len() != 4 {return 0;}
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^((202[0-9])|2030)$").unwrap();
+    }
+    if RE.is_match(field) {
+        return 1
+    } else {
+        //println!("Didn't Match {} for eyr", field);
+        return 0
+    }
+    //regex for year: ^((202[0-9])|2030)$
 }
 
 /**
@@ -38,7 +71,34 @@ fn check_eyr(field : &str) -> u8 {
  * Returns 1 if valid, 0 if invald
  */
 fn check_hgt(field : &str) -> u8 {
-    return 1;
+    if field.len() == 4 {
+        //in
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^(59|6[0-9]|7[0-6])in$").unwrap();
+        }
+        if RE.is_match(field) {
+            return 1
+        } else {
+            //println!("Didn't Match {} for hgt (in)", field);
+            return 0
+        }
+        //regex for in: ^(59|6[0-9]|7[0-6])in$
+    } else if field.len()==5 {
+        //cm
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^1([5-8][0-9]|9[0-3])cm$").unwrap();
+        }
+        if RE.is_match(field) {
+            return 1
+        } else {
+            //println!("Didn't Match {} for hgt (cm)", field);
+            return 0
+        }
+        //regex for cm: ^1([5-8][0-9]|9[0-3])cm$
+    } else {// bad length
+        //println!("Didn't Match {} for hgt (bad len)", field);
+        return 0
+    }
 }
 
 /**
@@ -48,7 +108,17 @@ fn check_hgt(field : &str) -> u8 {
  * Returns 1 if valid, 0 if invald
  */
 fn check_hcl(field : &str) -> u8 {
-    return 1;
+    if field.len() != 7 {return 0;}
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+    }
+    if RE.is_match(field) {
+        return 1
+    } else {
+        //println!("Didn't Match {} for hcl", field);
+        return 0
+    }
+    //regex for hair color: ^#[0-9a-f]{6}$
 }
 
 /**
@@ -58,7 +128,13 @@ fn check_hcl(field : &str) -> u8 {
  * Returns 1 if valid, 0 if invald
  */
 fn check_ecl(field : &str) -> u8 {
-    return 1;
+    match field {
+        "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => 1,
+        &_ => {
+            //println!("Didn't Match {} for ecl", field);
+            0
+        },
+    }
 }
 
 /**
@@ -68,7 +144,17 @@ fn check_ecl(field : &str) -> u8 {
  * Returns 1 if valid, 0 if invald
  */
 fn check_pid(field : &str) -> u8 {
-    return 1;
+    if field.len() != 9 {return 0;}
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^[0-9]{9}$").unwrap();
+    }
+    if RE.is_match(field) {
+        return 1
+    } else {
+        //println!("Didn't Match {} for pid", field);
+        return 0
+    }
+    //regex for pid: ^[0-9]{9}$
 }
 
 fn check_passport(block : &str) -> bool {
