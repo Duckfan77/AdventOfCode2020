cba06f30df535f84b93920470117816d4b40cf9b
Melissa <77photon@gmail.com>
2020-12-07 16:26:44 -0600

    Implement puzzle14
    Implemented puzzle14, by constructing a map of bag - subbag list rules, and then recursing over the map to find the total count.

--- puzzle14/src/main.rs
+++ puzzle14/src/main.rs
@@ -1,40 +1,67 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::HashSet;
+use std::collections::HashMap;
+
+//Recursively calculates the number of bags within the bag, counting itself
+fn bag_num(name: String, map: &HashMap<String, Vec<(i32, String)>>) -> i32 {
+    let mut count = 1;
+
+    println!("Examining {}", name);
+
+    let list = map.get(&name).unwrap();
+    for (n, bag) in list {
+        if *n == 0 {//empty list, just break out
+            break;
+        } else {
+            count += n * bag_num(String::from(bag), map);
+        }
+    }
+
+    return count
+}
 
 fn main() -> std::io::Result<()>{
     let mut file : File = File::open("input")?;
     let mut text = String::new();
     file.read_to_string(&mut text)?;
 
-    let mut total : HashSet<&str> = HashSet::new();
-    let mut looking : HashSet<&str> = HashSet::new();
-    let mut new : HashSet<&str> = HashSet::new();
-    let mut found = true;
-    looking.insert("shiny gold");
+    let mut map: HashMap<String, Vec<(i32, String)>> = HashMap::new();
 
-    while found {//find the bags that contain the new bag types to look for, so long as new ones are found
-        found = false;
-        new.clear();
-        for line in text.lines() {
-            let (name, rule) = line.split_at(line.find("contain").unwrap() - 6 as usize);
-            for i in looking.iter() {
-                if rule.contains(i) {
-                    if total.insert(name) {
-                        new.insert(name);
-                        found = true;
-                    }
-                }
+    for line in text.lines() {
+        let (name, rule) = line.split_at(line.find(" contain ").unwrap());
+        let (_, rule) = rule.split_at(9);
+
+        //replace bags with bag in all strings for better matching, remove trailing .
+        let name = name.replace("bags", "bag");
+        let rule = rule.replace("bags", "bag").replace(".", "");
+
+        //handle empty rules
+        if rule=="no other bag" {
+            map.insert(name, vec![(0, String::from(""))]);
+        }else {//handle other rules
+            let mut v:Vec<(i32, String)> = Vec::new();
+            //split at each ", " to get number bag sets
+            for subbag in rule.split(", ") {
+                //split the subbag to get number and bag, add to vector
+                let (n, bag) = subbag.split_at(1);
+                let n = n.parse::<i32>().unwrap_or(0);
+                let bag = bag.trim();
+                v.push((n, String::from(bag)));
+
+                //println!("{}, {}, {}", name, n, String::from(bag));
             }
-        }
-        //copy the new items into the looking set
-        looking.clear();
-        for i in new.iter(){
-            looking.insert(i);
+            //println!("Inserting key {}", name);
+            map.insert(name, v);
         }
     }
+/*
+    for (name, val) in (&map).iter() {
+        println!("{}, {:?}", name, val);
+    }*/
 
-    println!("There are {} options for top level bags", total.len());
+    let n = bag_num(String::from("shiny gold bag"), &map);
+
+    println!("The shiny gold bag consists of {} bags", n-1);
 
     Ok(())
 }
