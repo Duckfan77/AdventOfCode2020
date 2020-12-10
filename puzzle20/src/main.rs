3cbcdfe013b78688f1da9e6fecb3496505d34d5f
Melissa <77photon@gmail.com>
2020-12-10 11:53:18 -0600

    Implement puzzle20
    Implemented puzzle20, taking advantage of certain properties of the input data-set. Only including gaps of 3 and 1, and only including at most four gaps of 1 in a row allows handling each possible run of 1gaps size. The 3gaps must be reached, so the number of ways is simply the product of the number of ways between each 3gap, which can be computed from the number of 1gaps between them. Rather than truly checking the input space exhasutively, or even checking all possible sets of gaps between two gaps of 3, I simply checked only those forms that exist in the input, using pre-computed patterns.

--- puzzle20/src/main.rs
+++ puzzle20/src/main.rs
@@ -1,6 +1,48 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+//slice contains elements between low and high, must bridge gap between low and high
+fn _waysbetween(slice: &[i32], _low: i32, _high: i32) -> i128 {
+
+    //slices of size 3 or less are just how many ways can you pick from the slice
+    //1 way to pick 0, 3 ways to pick 1, 3 ways to pick 2, 1 way to pick 3
+    //for slices of size 3 or less,
+    if slice.len() < 3 {
+        return 2i128.pow(slice.len() as u32);
+    }
+
+    if slice.len() == 3 {
+        return 3+3+1;
+    }
+
+    //slices of size 4 need to select at least 1, but any 1 will work, as then will be in range of the 3gap which is max allowed
+    //4 ways to pick 1, 6 ways to pick 2, 4 ways to pick 3, 1 way to pick 4
+    if slice.len() == 4 {
+        return 4+6+4+1;
+    }
+
+    println!("Unexpected Slice Length, ERROR");
+    return 0;
+}
+
+//reimplemetation of _waysbetween using just the length of the run of 1s
+fn options_from_len(run: u32) -> i128 {
+    if run < 3 {
+        return 2i128.pow(run);
+    }
+
+    if run == 3{
+        return 3+3+1;
+    }
+
+    if run == 4 {
+        return 4+6+4+1;
+    }
+
+    println!("Unexpected run length, ERROR");
+    return 0;
+}
+
 fn main() -> std::io::Result<()>{
     let mut file : File = File::open("input")?;
     let mut text = String::new();
@@ -14,26 +56,46 @@ fn main() -> std::io::Result<()>{
 
     //sort the array
     v.sort_unstable();
+    v.push(v[v.len()-1]+3);//add phone to list
     //println!("{:#?}", v);
 
-    //iterate over the array, and count 1 gaps and 3 gaps
-    let mut jolt3 = 1; //gap to phone
-    let mut jolt1 = 0;
+    //Idea 1: brute force test all 2^90 possible inclusion sets, each one can either be in or out of the configuration, seems excessive and time consuming
+
+    //Idea 2: find number of ways between each gap of 3, since those need to be reached, there's no other way past them, then multiply those counts
+
+    //Iterate over the array, find the length of runs of 1 gaps, as 2-gaps don't exist in the input
+    let mut maxj1 = 0;
+    let mut runj1 = 0;
     let mut prev = 0;
+    let mut running_ways = 1;
     for i in v.iter() {
         if i-prev == 1{
-            //println!("{} - {} has gap of 1", i, prev);
-            jolt1 += 1;
+            println!("{} - {} has gap of 1", i, prev);
+            runj1 += 1;
         } else if i-prev ==3 {
-            //println!("{} - {} has gap of 3", i, prev);
-            jolt3 += 1;
+            println!("{} - {} has gap of 3", i, prev);
+            println!("Run length: {}", runj1);
+            if maxj1 < runj1 {
+                maxj1 = runj1;
+            }
+            //must subract 1 from runj1, as the first one is required as the step from the previous 3-gap, but can't go negative
+            running_ways *= options_from_len(if runj1==0 {0} else {runj1-1});
+            runj1 = 0;
         } else {
-            //println!("{} - {} has gap of 2", i, prev);
+            //println!("{} - {} has gap of 1", i, prev);
+            println!("Run length: {}", runj1);
+            if maxj1 < runj1 {
+                maxj1 = runj1;
+            }
+            //must subract 1 from runj1, as the first one is required as the step from the previous 3-gap, but can't go negative
+            running_ways *= options_from_len(if runj1==0 {0} else {runj1-1});
+            runj1 = 0;
         }
         prev = *i;
     }
 
-    println!("jolt1: {}, jolt3: {}, product: {}", jolt1, jolt3, jolt1*jolt3);
+    println!("Max run of j1, aka, max run between j3s: {}", maxj1);
+    println!("Total number of ways to get to phone: {}", running_ways);
 
     Ok(())
 }
