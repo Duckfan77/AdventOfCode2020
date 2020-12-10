262761634ea0324d0a9426aa85aeadd7ab3f77a9
Melissa <77photon@gmail.com>
2020-12-10 09:37:15 -0600

    Create puzzle19 and puzzle20

---
+++ puzzle19/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()>{
+    let mut file : File = File::open("input")?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
+
+
+    Ok(())
+}
