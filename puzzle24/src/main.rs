5a5e8be10f14de1a411dbf2efd9b18d183731f26
Melissa <77photon@gmail.com>
2020-12-12 12:31:49 -0600

    Implement puzzle24
    Implemented puzzle24 using a pair of waypoint variables and a pair of ship location variables, and then moving both according to the rules. The waypoint is measured relative to the ship, so only one set changes on any given instruction.

--- puzzle24/src/main.rs
+++ puzzle24/src/main.rs
@@ -1,86 +1,74 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-#[derive(Debug)]
-enum Dir {
-    East,
-    North,
-    South,
-    West
-}
-
 fn main() -> std::io::Result<()>{
     let mut file : File = File::open("input")?;
     let mut text = String::new();
     file.read_to_string(&mut text)?;
 
-    let mut dir = Dir::East;
     let mut x = 0;
     let mut y = 0;
+    let mut wayx = 10;
+    let mut wayy = 1;
 
     for line in text.lines() {
         let (op, val) = line.split_at(1);
         let val = val.parse::<i32>().unwrap_or(0);
 
-        println!("x: {}, y: {}, dir: {:?}", x, y, dir);
+        println!("x: {}, y: {}, wayx: {}, wayy: {}", x, y, wayx, wayy);
 
         match op {
-            "N" => y += val,
-            "S" => y -= val,
-            "E" => x += val,
-            "W" => x -= val,
+            "N" => wayy += val,
+            "S" => wayy -= val,
+            "E" => wayx += val,
+            "W" => wayx -= val,
             "F" => {
-                match dir {
-                    Dir::East => x += val,
-                    Dir::North => y += val,
-                    Dir::South => y -= val,
-                    Dir::West => x -= val,
-                }
-            }
-            "R" => {
-                dir = match (dir, val) {
-                    (Dir::East, 90) => Dir::South,
-                    (Dir::East, 180) => Dir::West,
-                    (Dir::East, 270) => Dir::North,
-                    (Dir::East, _)  => Dir::East,
-
-                    (Dir::North, 90) => Dir::East,
-                    (Dir::North, 180) => Dir::South,
-                    (Dir::North, 270) => Dir::West,
-                    (Dir::North, _)  => Dir::North,
-
-                    (Dir::South, 90) => Dir::West,
-                    (Dir::South, 180) => Dir::North,
-                    (Dir::South, 270) => Dir::East,
-                    (Dir::South, _)  => Dir::South,
-
-                    (Dir::West, 90) => Dir::North,
-                    (Dir::West, 180) => Dir::East,
-                    (Dir::West, 270) => Dir::South,
-                    (Dir::West, _)  => Dir::West,
-                }
+                x += wayx*val;
+                y += wayy*val;
             }
             "L" => {
-                dir = match (dir, val) {
-                    (Dir::East, 90) => Dir::North,
-                    (Dir::East, 180) => Dir::West,
-                    (Dir::East, 270) => Dir::South,
-                    (Dir::East, _)  => Dir::East,
+                match val {
+                    90 => {
+                        let tmp = wayx;
+                        wayx = -wayy;
+                        wayy = tmp;
+                    }
 
-                    (Dir::North, 90) => Dir::West,
-                    (Dir::North, 180) => Dir::South,
-                    (Dir::North, 270) => Dir::East,
-                    (Dir::North, _)  => Dir::North,
+                    180 => {
+                        wayx = -wayx;
+                        wayy = -wayy;
+                    }
 
-                    (Dir::South, 90) => Dir::East,
-                    (Dir::South, 180) => Dir::North,
-                    (Dir::South, 270) => Dir::West,
-                    (Dir::South, _)  => Dir::South,
+                    270 => {
+                        let tmp = wayx;
+                        wayx = wayy;
+                        wayy = -tmp;
+                    }
 
-                    (Dir::West, 90) => Dir::South,
-                    (Dir::West, 180) => Dir::East,
-                    (Dir::West, 270) => Dir::North,
-                    (Dir::West, _)  => Dir::West,
+                    _ => ()
+                }
+
+            }
+            "R" => {
+                match val {
+                    270 => {
+                        let tmp = wayx;
+                        wayx = -wayy;
+                        wayy = tmp;
+                    }
+
+                    180 => {
+                        wayx = -wayx;
+                        wayy = -wayy;
+                    }
+
+                    90 => {
+                        let tmp = wayx;
+                        wayx = wayy;
+                        wayy = -tmp;
+                    }
+
+                    _ => ()
                 }
             }
 
@@ -88,7 +76,7 @@ fn main() -> std::io::Result<()>{
         }
     }
 
-    println!("x: {}, y: {}, dir: {:?}", x, y, dir);
+    println!("x: {}, y: {}, wayx: {}, wayy: {}", x, y, wayx, wayy);
 
     println!("Manhatten distance: {}", x.abs() + y.abs());
 
