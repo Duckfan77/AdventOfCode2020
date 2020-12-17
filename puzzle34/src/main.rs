728a5837190141b984147372b9b1813e63be397b
Melissa <77photon@gmail.com>
2020-12-17 10:38:34 -0600

    Implement puzzle34
    Implemented puzzle34, by adding a fourth dimension to the array. Ran into stack overflow issues, so the arrays were replaced with nested Vecs, to keep the memory on the heap. Not efficient, release build runs in just over 1 second.

--- puzzle34/src/main.rs
+++ puzzle34/src/main.rs
@@ -1,49 +1,55 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-const SIZE: usize = 50;
-const CENTER: usize = SIZE/2;
+const SIZE1: usize = 30;
+const SIZE2: usize = 30;
+const CENTER1: usize = SIZE1/2;
+const CENTER2: usize = SIZE2/2;
 
-fn iterate_array(array: [[[bool; SIZE]; SIZE]; SIZE]) -> ([[[bool; SIZE]; SIZE]; SIZE], bool) {
+fn iterate_array(array: Vec<Vec<Vec<Vec<bool>>>>) -> (Vec<Vec<Vec<Vec<bool>>>>, bool) {
 
-    let mut outarray: [[[bool; SIZE]; SIZE]; SIZE] = [[[false; SIZE]; SIZE]; SIZE];
+    let mut outarray = vec![vec![vec![vec![false; SIZE2]; SIZE2]; SIZE1]; SIZE1];
     let mut changed = false;
 
-    for i in 1..(SIZE-1) as i32 {
-        for j in 1..(SIZE-1) as i32 {
-            for k in 1..(SIZE-1) as i32 {
-                //get number full around the cell
-                let mut countfull = 0;
-                for xoff in -1..2 {
-                    for yoff in -1..2 {
-                        for zoff in -1..2 {
-                            //don't look at 0,0,0
-                            if xoff==0 && yoff==0 && zoff==0 {
-                                continue;
+    for i in 1..(SIZE2-1) as i32 {
+        for j in 1..(SIZE2-1) as i32 {
+            for k in 1..(SIZE1-1) as i32 {
+                for l in 1..(SIZE1-1) as i32 {
+                    //get number full around the cell
+                    let mut countfull = 0;
+                    for xoff in -1..2 {
+                        for yoff in -1..2 {
+                            for zoff in -1..2 {
+                                for woff in -1..2 {
+                                    //don't look at 0,0,0,0
+                                    if xoff==0 && yoff==0 && zoff==0 && woff==0{
+                                        continue;
+                                    }
+                                    //println!("i: {}, j: {}, k: {}, l: {}", i, j, k, l);
+                                    countfull += if array[(i+xoff) as usize][(j+yoff) as usize][(k+zoff) as usize][(l+woff) as usize] {1} else {0};
+                                }
                             }
-
-                            countfull += if array[(i+xoff) as usize][(j+yoff) as usize][(k+zoff) as usize] {1} else {0};
                         }
                     }
-                }
 
-                //implement transition rules, track if a state change has happenes
-                outarray[i as usize][j as usize][k as usize] = match array[i as usize][j as usize][k as usize] {
-                    true => {
-                        if countfull == 2 || countfull == 3 {
-                            true
-                        } else {
-                            changed = true;
-                            false
-                        }
-                    },
+                    //implement transition rules, track if a state change has happenes
+                    outarray[i as usize][j as usize][k as usize][l as usize] = match array[i as usize][j as usize][k as usize][l as usize] {
+                        true => {
+                            if countfull == 2 || countfull == 3 {
+                                true
+                            } else {
+                                changed = true;
+                                false
+                            }
+                        },
 
-                    false => {
-                        if countfull == 3 {
-                            changed = true;
-                            true
-                        } else {
-                            false
+                        false => {
+                            if countfull == 3 {
+                                changed = true;
+                                true
+                            } else {
+                                false
+                            }
                         }
                     }
                 }
@@ -61,12 +67,13 @@ fn main() -> std::io::Result<()>{
     file.read_to_string(&mut text)?;
 
     //initialize array to empty
-    let mut space: [[[bool; SIZE]; SIZE]; SIZE] = [[[false; SIZE]; SIZE]; SIZE];
+    let mut space = vec![vec![vec![vec![false; SIZE2]; SIZE2]; SIZE1]; SIZE1];
+    //let mut space: Box<[[[[bool; SIZE2]; SIZE2]; SIZE1]; SIZE1]> = Box::new([[[[false; SIZE2]; SIZE2]; SIZE1]; SIZE1]);
 
     //populate space from file
     for (i, line) in text.lines().enumerate() {
         for (j, c) in line.chars().enumerate() {
-            space[CENTER+i][CENTER+j][CENTER] = match c {
+            space[CENTER2+i][CENTER2+j][CENTER1][CENTER1] = match c {
                 '.' => false,
                 '#' => true,
                  _  => panic!("Unexpected character in test: {}", c),
@@ -83,10 +90,12 @@ fn main() -> std::io::Result<()>{
 
     //count active cells at the end
     let mut count = 0;
-    for slice in space.iter() {
-        for row in slice.iter() {
-            for cell in row.iter() {
-                count += if *cell {1} else {0}
+    for cube in space.iter(){
+        for slice in cube.iter() {
+            for row in slice.iter() {
+                for cell in row.iter() {
+                    count += if *cell {1} else {0}
+                }
             }
         }
     }
