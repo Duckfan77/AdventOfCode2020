6b0cf79be13869610337b5ed801db08f92fe836a
Melissa <77photon@gmail.com>
2020-12-24 16:41:55 -0600

    Implement day 23-2
    Implemented day 23-2 using the strategy suggested on the AoC subreddit, using a second list to describe a linked list of next cell in the loop.

--- day23-2/src/main.rs
+++ day23-2/src/main.rs
@@ -3,61 +3,60 @@ fn main() -> std::io::Result<()>{
     for v in 10..1000001 {
         cups.push(v);
     }
-    println!("Number of cups total: {}", cups.len());
-    let mut removed: Vec<i32> = Vec::new();
+    println!("Number of cups total: {}, {:?}", cups.len(), cups[cups.len()-1]);
 
-    let mut cur=0;
+    let max = *cups.iter().max().unwrap();
+    let min = *cups.iter().min().unwrap();
+
+    //construct a vec to serve as a "linked list" telling you where each cup points one clockwise
+    let mut list = vec![0; max+1];
+    (0..cups.len()).for_each(|i| list[cups[i]] = cups[(i+1) % cups.len()]);
+
+    let mut cur=cups[0];
     for i in 1..10000001 {
         if i%10000 == 0 {
             println!("turn {}", i);
         }
-        let cur_label = cups[cur];
 
         //println!("-- move {} --\ncups: {:?}\ncurrent index: {}, current label: {}", i, cups, cur, cur_label);
 
         //remove 3
-        for _ in 0..3 {
-            if cur+1 < cups.len() {
-                removed.push(cups.remove(cur+1));
-            } else {
-                removed.push(cups.remove(0));
-            }
-        }
+        //get the three to be removed
+        let p1 = list[cur];
+        let p2 = list[p1];
+        let p3 = list[p2];
+
+        //skip the three removed in the circle
+        list[cur] = list[p3];
 
         //println!("Pick up: {:?}", removed);
 
         //select destination
-        let mut dest = cur_label-1;
-        if dest < 1 {
-            dest = 9;
+        let mut dest = cur-1;
+        if dest < min {
+            dest = max;
         }
-        while removed.contains(&dest) {
+        while [p1, p2, p3].contains(&dest) {
             dest -= 1;
-            if dest < 1 {
-                dest = 9;
+            if dest < min {
+                dest = max;
             }
         }
 
         //println!("destination: {}\n", dest);
 
         //Insert cups
-        let dest_addr = cups.iter().position(|x| *x == dest).unwrap()+1;
-        for c in removed.drain(..).rev() {
-            cups.insert(dest_addr, c);
-        }
+        list[p3] = list[dest];
+        list[dest] = p1;
 
         //update current
-        cur = cups.iter().position(|x| *x == cur_label).unwrap()+1;
-        if cur >= cups.len() {
-            cur = 0;
-        }
+        cur = list[cur];
     }
 
     //println!{"-- final --\ncups: {:?}, current index: {}", cups, cur};
-    let pos1 = cups.iter().position(|x| *x == 1).unwrap();
-    let r = cups[pos1+1];
-    let rr = cups[pos1+2];
-    println!("r: {}, rr: {}", r, rr);
+    let r = list[1];
+    let rr = list[r];
+    println!("r: {}, rr: {}, product: {}", r, rr, r*rr);
 
     Ok(())
 }
