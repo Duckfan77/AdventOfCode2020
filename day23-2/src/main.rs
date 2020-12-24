f4b7e3af8dbd6a371d41f8aa7714ab7132911405
Melissa <77photon@gmail.com>
2020-12-24 12:56:23 -0600

    Attempt to implement day23-2
    Attempt at implementing day 23-2 by using the method from part 1 and running it for the total length. Did not yeild the correct answer, will examine again later.

--- day23-2/src/main.rs
+++ day23-2/src/main.rs
@@ -1,12 +1,19 @@
 fn main() -> std::io::Result<()>{
     let mut cups = vec![2,8,4,5,7,3,9,6,1];
+    for v in 10..1000001 {
+        cups.push(v);
+    }
+    println!("Number of cups total: {}", cups.len());
     let mut removed: Vec<i32> = Vec::new();
 
     let mut cur=0;
-    for i in 1..101 {
+    for i in 1..10000001 {
+        if i%10000 == 0 {
+            println!("turn {}", i);
+        }
         let cur_label = cups[cur];
 
-        println!("-- move {} --\ncups: {:?}\ncurrent index: {}, current label: {}", i, cups, cur, cur_label);
+        //println!("-- move {} --\ncups: {:?}\ncurrent index: {}, current label: {}", i, cups, cur, cur_label);
 
         //remove 3
         for _ in 0..3 {
@@ -17,7 +24,7 @@ fn main() -> std::io::Result<()>{
             }
         }
 
-        println!("Pick up: {:?}", removed);
+        //println!("Pick up: {:?}", removed);
 
         //select destination
         let mut dest = cur_label-1;
@@ -31,7 +38,7 @@ fn main() -> std::io::Result<()>{
             }
         }
 
-        println!("destination: {}\n", dest);
+        //println!("destination: {}\n", dest);
 
         //Insert cups
         let dest_addr = cups.iter().position(|x| *x == dest).unwrap()+1;
@@ -46,7 +53,11 @@ fn main() -> std::io::Result<()>{
         }
     }
 
-    println!{"-- final --\ncups: {:?}, current index: {}", cups, cur};
+    //println!{"-- final --\ncups: {:?}, current index: {}", cups, cur};
+    let pos1 = cups.iter().position(|x| *x == 1).unwrap();
+    let r = cups[pos1+1];
+    let rr = cups[pos1+2];
+    println!("r: {}, rr: {}", r, rr);
 
     Ok(())
 }
