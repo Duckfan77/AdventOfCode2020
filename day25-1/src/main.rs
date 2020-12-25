159f8ffa33f205a32b32a897f051f7ed8d507849
Melissa <77photon@gmail.com>
2020-12-25 11:42:30 -0600

    Implement day25-1
    Implemented day25-1 by looping to find the loop number used, and then using that to run that many cycles on the other number.

--- day25-1/src/main.rs
+++ day25-1/src/main.rs
@@ -1,3 +1,27 @@
+const SUBJECT_NUMBER: u64 = 7;
+
 fn main() {
-    println!("Hello, world!");
+    let server_public = 14082811;
+    let client_public = 5249543;
+
+    //find client loop size
+    let mut ls = 0;
+    let mut val: u64 = 1;
+    loop {
+        val = (val*SUBJECT_NUMBER)%20201227;
+        ls += 1;
+        if val == client_public {
+            break;
+        }
+    }
+
+    println!("Client loop number: {}", ls);
+
+    val = 1;
+    for _i in 0..ls {
+        //println!("Cycle: {}", _i);
+        val = (val*server_public)%20201227;
+    }
+
+    println!("Total value: {}", val);
 }
